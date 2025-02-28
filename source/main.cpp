35f3cb9a5960717e77d7aa52870647cd3fc48bc1
Alan <alanhaugen@gmail.com>
2025-02-28 13:19:36 +0100

    More or less finished the compulsory........ except it isn't vulkan yet. Missing door logic

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,100 @@
 
 int bullets;
 
+class BezierCurve
+{
+private:
+    int d;
+
+public:
+    glm::vec3 c[4];
+
+    BezierCurve()
+    {
+        d = 4;
+    }
+
+    // deCasteljau's algorithm for evaluating Bezier curves
+    glm::vec3 EvaluateBezier(float t)
+    {
+        glm::vec3 a[4]; // 4=d+1 for kubisk Bezier for (int i=0; i<4; i++)
+
+        for (int i=0; i < 4; i++)
+        {
+            a[i] = c[i];
+        }
+
+        for (int k = d; k > 0; k--) //for (int k=1; k<=d; k++) {
+        {
+            for (int i = 0; i < k; i++) //for (int i=0; i<=d=k; i++) a[i] = a[i] * (1=t) + a[i+1] * t;
+            {
+                a[i] = a[i] * (1 - t) + a[i + 1] * t;
+            }
+        }
+
+        return a[0];
+    }
+};
+
+
+class Enemy : public Cube
+{
+private:
+    bool goRight;
+    float tick;
+    BezierCurve path;
+
+public:
+    Enemy(float x, float y, float z, float length, float width, float height)
+        : Cube(x, y, z, length, width, height)
+    {
+        goRight = true;
+        tick = 0.0f;
+
+        path.c[0] = glm::vec3(-6, 0, 0);
+        path.c[1] = glm::vec3(0, 4, 0);
+        path.c[2] = glm::vec3(3, 4, 0);
+        path.c[3] = glm::vec3(6, 0, 0);
+    }
+
+    void Update()
+    {
+        Cube::Update();
+
+        tick += 0.01;
+
+        if (tick > 1.0)
+        {
+            tick = 0.0;
+        }
+
+        *matrix.x = path.EvaluateBezier(tick).x;
+        *matrix.y = path.EvaluateBezier(tick).y;
+        //*matrix.z = path.EvaluateBezier(tick).z;
+
+        /*if (*matrix.x > 9.0f)
+        {
+            goRight = false;
+        }
+
+        if (*matrix.x < -5.0f)
+        {
+            goRight = true;
+        }
+
+        if (goRight)
+        {
+            *matrix.x += 0.1;
+        }
+        else
+        {
+            *matrix.x -= 0.1;
+        }
+
+        *matrix.y += sin(tick) / 10;*/
+    }
+};
+
 class City : public IScene
 {
 private:
@@ -36,7 +130,7 @@ public:
         player->Uniform("colour", glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
         player->tag = "player";
         player->collisionBox->type = "player";
-        title = new Text("You have picked up: " + String(bullets) + " / 8 pickups");
+        title = new Text("You have picked up: " + String(bullets) + " / 9 pickups");
         cam = new Camera(glm::vec3(0,0,0), glm::vec3(0,1,0), glm::vec3(0,0.5,-1), 75);
 
         for (int i = 0; i < 8; i++)
@@ -51,7 +145,7 @@ public:
 
         for (int i = 0; i < 2; i++)
         {
-            Cube* enemy = new Cube(5.0f - random.RandomRange(0, 10), 5.0f - random.RandomRange(0, 10), -4, 0.1, 0.1, 0.1);
+            Enemy* enemy = new Enemy(5.0f - random.RandomRange(0, 10), 5.0f - random.RandomRange(0, 10), -4, 0.1, 0.1, 0.1);
             enemy->Uniform("colour", glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
             enemy->tag = "pickup";
             enemies.Add(enemy);
@@ -110,8 +204,18 @@ public:
                 if (physics->Collide(pickups[i]->collisionBox, "player"))
                 {
                     bullets++;
-                    delete title;
-                    title = new Text("You have picked up: " + String(bullets) + " / 8 pickups");
+
+                    if (bullets >= 9)
+                    {
+                        delete title;
+                        title = new Text("You Won");
+                    }
+                    else
+                    {
+                        delete title;
+                        title = new Text("You have picked up: " + String(bullets) + " / 9 pickups");
+                    }
+
                     pickups[i]->Hide();
                 }
             }
@@ -131,36 +235,146 @@ public:
 
         if (physics->Collide(door->collisionBox, "player"))
         {
-            Application::NextScene();
+            //Application::NextScene();
         }
     }
 };
 
 class House : public IScene
 {
-public:
+private:
     Camera* cam;
+    Cube* player;
+    Text* title;
+    Cube* pickup;
 
+public:
     void Init()
     {
         cam = new Camera(glm::vec3(0,0,0), glm::vec3(0,1,0), glm::vec3(0,0.5,-1), 75);
         components.Add(cam);
+
+        player = new Cube(0, 0, -4, 0.1, 0.1, 0.1);
+        player->Uniform("colour", glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
+        player->tag = "player";
+        player->collisionBox->type = "player";
+        components.Add(player);
+
+        pickup = new Cube(0,2.5, -4, 0.1, 0.1, 0.1);
+        pickup->Uniform("colour", glm::vec4(1.0f, 1.0f, 0.0f, 1.0f));
+        pickup->tag = "pickup";
+        pickup->collisionBox->type = "pickup";
+        components.Add(pickup);
+
+        title = new Text("You have picked up: " + String(bullets) + " / 9 pickups");
     }
 
     void Update()
     {
+        if (input.Held(input.Key.A))
+        {
+            *player->matrix.x -= 0.1f;
+        }
+        if (input.Held(input.Key.D))
+        {
+            *player->matrix.x += 0.1f;
+        }
+        if (input.Held(input.Key.W))
+        {
+            *player->matrix.y += 0.1f;
+        }
+        if (input.Held(input.Key.S))
+        {
+            *player->matrix.y -= 0.1f;
+        }
+
+        title->Update();
     }
 
     void UpdateAfterPhysics()
     {
+        if (physics->Collide(pickup->collisionBox, "player") && pickup->isVisible())
+        {
+            bullets++;
+
+            if (bullets >= 9)
+            {
+                delete title;
+                title = new Text("You Won");
+            }
+            else
+            {
+                delete title;
+                title = new Text("You have picked up: " + String(bullets) + " / 9 pickups");
+            }
+
+            pickup->Hide();
+        }
+    }
+};
+
+class Game : public IScene
+{
+private:
+    IScene* city;
+    IScene* house;
+    IScene* activeScene;
+    bool isInCity;
+
+public:
+    void Init()
+    {
+        city  = new City();
+        house = new House();
+
+        city->Init();
+        house->Init();
+
+        isInCity = true;
+    }
+
+    void Update()
+    {
+        if (isInCity)
+        {
+            activeScene = city;
+        }
+        else
+        {
+            activeScene = house;
+        }
+
+        if (input.Pressed(input.Key.SPACE))
+        {
+            isInCity = !isInCity;
+        }
+
+        // Run the game logic
+        activeScene->Update();
+
+        // Update game components
+        for (unsigned int i = 0; i < activeScene->components.Size(); i++)
+        {
+            (*activeScene->components[i])->Update();
+        }
+    }
+
+    void UpdateAfterPhysics()
+    {
+        activeScene->UpdateAfterPhysics();
+
+        // Update game components after physics
+        for (unsigned int i = 0; i < activeScene->components.Size(); i++)
+        {
+            (*activeScene->components[i])->UpdateAfterPhysics();
+        }
     }
 };
 
 int main(int argc, char **argv)
 {
     Application application(argc, argv);
-    application.AddScene(new City);
-    application.AddScene(new House);
+    application.AddScene(new Game);
 
     return application.Exec();
 }
